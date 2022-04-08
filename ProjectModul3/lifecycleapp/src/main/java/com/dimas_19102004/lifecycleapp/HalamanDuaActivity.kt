62fbbeedbe40131cbc04c811196cf4984297b5c6
gumerang26 <19102004@ittelkom-pwt.ac.id>
2022-04-08 13:40:55 +0700

    Modul Praktikum 3

---
+++ ProjectModul3/lifecycleapp/src/main/java/com/dimas_19102004/lifecycleapp/HalamanDuaActivity.kt
@@ -0,0 +1,11 @@
+package com.dimas_19102004.lifecycleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanDuaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_dua)
+    }
+}
