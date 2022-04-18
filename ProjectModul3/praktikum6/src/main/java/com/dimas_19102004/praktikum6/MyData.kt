9d4cc82254c6b64b9cee36b94cbd45d1a366a68a
gumerang26 <19102004@ittelkom-pwt.ac.id>
2022-04-18 19:51:40 +0700

    Modul Praktikum 6

---
+++ ProjectModul3/praktikum6/src/main/java/com/dimas_19102004/praktikum6/MyData.kt
@@ -0,0 +1,12 @@
+package com.dimas_19102004.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String )
+    : Parcelable
