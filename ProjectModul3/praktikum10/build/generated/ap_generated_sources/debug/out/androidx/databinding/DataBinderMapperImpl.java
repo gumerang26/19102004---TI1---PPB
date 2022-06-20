edac8f65befd5c6436638a5d64949544a9e1bd07
gumerang26 <19102004@ittelkom-pwt.ac.id>
2022-06-20 17:01:20 +0700

    Modul Praktikum 10

---
+++ ProjectModul3/praktikum10/build/generated/ap_generated_sources/debug/out/androidx/databinding/DataBinderMapperImpl.java
@@ -0,0 +1,7 @@
+package androidx.databinding;
+
+public class DataBinderMapperImpl extends MergedDataBinderMapper {
+  DataBinderMapperImpl() {
+    addMapper(new com.dimas_19102004.praktikum10.DataBinderMapperImpl());
+  }
+}
---
+++ ProjectModul3/praktikum10/build/generated/data_binding_trigger/debug/com/dimas_19102004/praktikum10/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.dimas_19102004.praktikum10;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
