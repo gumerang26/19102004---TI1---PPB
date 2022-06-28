0c2cbcae59718703f3db276bd5c63a275f474672
gumerang26 <19102004@ittelkom-pwt.ac.id>
2022-06-28 23:45:40 +0700

    Modul Praktikum 11

---
+++ ProjectModul3/praktikum11/build/generated/ap_generated_sources/debug/out/com/dimas_19102004/praktikum11/BR.java
@@ -0,0 +1,5 @@
+package com.dimas_19102004.praktikum11;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ ProjectModul3/praktikum11/build/generated/data_binding_trigger/debug/com/dimas_19102004/praktikum11/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.dimas_19102004.praktikum11;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
